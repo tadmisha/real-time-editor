5ad27bee12448cc98d799d5a3d81c6d29872b024
tadmisha <tadmisha@gmail.com>
2024-10-27 11:43:30 +0100

    Decorator to repeat if function is incorrect

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 
+# & Decorator to check if input is correct
+def repeat_if_incorrect(func):    
+    def wrapper():
+        function_return = func()
+        while (not function_return):
+            function_return = func()
+        return function_return
+            
+    return wrapper
+
+
 # & Main function
 def main():
     ...
